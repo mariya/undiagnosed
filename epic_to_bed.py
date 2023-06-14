c56aa351701a3b4c9fc4671f21f992565bb1fcd9
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-06-14 12:31:12 +0200

    Add liftover to hg19

--- epic_to_bed.py
+++ epic_to_bed.py
@@ -1,18 +1,70 @@
 import csv
 import random
+from pyliftover import LiftOver
 
 EPIC_DIR="../methylation/files/WE-3687_230602_ResultReport"
 EPIC_TSV="%s/WE-3687_230602_SampleMethylationProfile.txt"%EPIC_DIR
 OUT_DIR="./out"
+LIFTOVER = LiftOver('hg38', 'hg19')
 
-def append_bed_line(bed_file, chr, start, end, id, beta_value):
+HG19 = "hg19"
+HG38 = "hg38"
+
+def bed_filename(sample_name, ref_genome_version, sorted=False):
+    sorted = "sorted" if sorted else "unsorted"
+    return "%s/%s.%s.%s.bed" % (OUT_DIR, sample, ref_genome_version, sorted)
+    
+def to_hg19(chr, pos):
+    converted = LIFTOVER.convert_coordinate(chr, pos)
     try:
-        if float(start) > 0 and float(end) > 0:
-            line = '\t'.join([chr, start, end, id, beta_value])
-            with open(bed_file, 'a') as file:
-                file.write(line + '\n')
+        return converted[0][1]
     except:
+        print("Could not find hg19 coordinates for hg38 coordinates %s:%s" % (chr, pos))
+        return 0
+
+def append_bed_line(sample, chr, start, end, id, beta_value):
+    bed_file_hg38 = bed_filename(sample, HG38)
+    bed_file_hg19 = bed_filename(sample, HG19)
+    
+    try:
+        i_start = int(start)
+        i_end = int(end)
+        if i_start > 0 and i_end > 0:
+            # hg38
+            line = '\t'.join([chr, start, end, id, beta_value])
+            with open(bed_file_hg38, 'a') as file:
+                file.write(line + '\n')
+
+            # hg19 conversion
+            hg19_start = to_hg19(chr, i_start)
+            hg19_end = to_hg19(chr, i_end)
+            if hg19_start > 0 and hg19_end > 0:
+                line = '\t'.join([chr, f"{hg19_start}", f"{hg19_end}", id, beta_value])
+                with open(bed_file_hg19, 'a') as file:
+                    file.write(line + '\n')
+    except ValueError:
         return
+
+def sort_bed_file(sample, ref_genome_version):
+    unsorted_bed_file = bed_filename(sample, ref_genome_version, False)
+    sorted_bed_file = bed_filename(sample, ref_genome_version, True)
+
+    # Add an IGV track line to each bed file, in a random color
+    r = "%s" % random.randint(0,255)
+    g = "%s" % random.randint(0,255)
+    b = "%s" % random.randint(0,255)
+    rgb = ','.join([r,g,b])
+
+    track_line = 'track name="%s EPIC" description="%s EPIC" graphType="bar" color=%s viewLimits=0:1 maxHeightPixels=50:50:50' % (sample, sample, rgb)
+    with open(sorted_bed_file, 'w') as file:
+        file.write(track_line + '\n')
+
+    with open(unsorted_bed_file, 'r') as file:
+        lines = file.readlines()
+        sorted_lines = sorted(lines, key=lambda x: (x.split('\t')[0], int(x.split('\t')[1])))
+        with open(sorted_bed_file, 'a') as file:
+            file.writelines(sorted_lines)
+
 
 with open(EPIC_TSV, newline='') as tsvfile:
     tsvreader = csv.reader(tsvfile, delimiter='\t')
@@ -47,7 +99,6 @@ with open(EPIC_TSV, newline='') as tsvfile:
 
         # Loop through all samples
         for sample, beta_col in beta_columns.items():
-            bed_file = "%s/%s.unsorted.bed" % (OUT_DIR, sample)
             beta_value = row[beta_col]
 
             # Discard any rows with invalid beta value
@@ -60,35 +111,18 @@ with open(EPIC_TSV, newline='') as tsvfile:
                 # UCSC_CPG_ISLANDS_NAME
                 # If this field is blank, use pos for start and end
                 if islands.strip() == "":
-                    append_bed_line(bed_file, chr, pos, pos, id, beta_value)
+                    append_bed_line(sample, chr, pos, pos, id, beta_value)
                 else:
                     # Format: chr18:23503197-23504189
                     # or: chr6:6006456-6006810;chr6:6007154-6007564;chr6:6008624-6009364
                     islands_list = islands.split(';')
                     for isle in islands_list:
                         positions = isle.split(":")[1].split("-")
-                        append_bed_line(bed_file, chr, positions[0], positions[1], id, beta_value)
+                        append_bed_line(sample, chr, positions[0], positions[1], id, beta_value)
 
     # Sort all the bedfiles
     print("\n\nSorting BED files...")
     for sample, beta_col in beta_columns.items():
-        unsorted_bed_file = "%s/%s.unsorted.bed" % (OUT_DIR, sample)
-        sorted_bed_file = "%s/%s.sorted.bed" % (OUT_DIR, sample)
-
-        # Add an IGV track line to each bed file, in a random color
-        r = "%s" % random.randint(0,255)
-        g = "%s" % random.randint(0,255)
-        b = "%s" % random.randint(0,255)
-        rgb = ','.join([r,g,b])
-
-        track_line = 'track name="%s EPIC" description="%s EPIC" graphType="bar" color=%s viewLimits=0:1 maxHeightPixels=50:50:50' % (sample, sample, rgb)
-        with open(sorted_bed_file, 'w') as file:
-            file.write(track_line + '\n')
-
-        with open(unsorted_bed_file, 'r') as file:
-            lines = file.readlines()
-            sorted_lines = sorted(lines, key=lambda x: (x.split('\t')[0], int(x.split('\t')[1])))
-            with open(sorted_bed_file, 'a') as file:
-                file.writelines(sorted_lines)
-
+        sort_bed_file(sample, HG38)
+        sort_bed_file(sample, HG19)
 
