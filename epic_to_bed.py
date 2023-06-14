dd11b2e96b2d7dd5a00d95533d51e8e6d7e93577
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-06-14 09:30:08 +0200

    Fix IGV track line in sorted bed files

--- epic_to_bed.py
+++ epic_to_bed.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 EPIC_DIR="../methylation/files/WE-3687_230602_ResultReport"
 EPIC_TSV="%s/WE-3687_230602_SampleMethylationProfile.txt"%EPIC_DIR
@@ -11,6 +12,7 @@ with open(EPIC_TSV, newline='') as tsvfile:
     header = next(tsvreader)
 
     # Get the relevant sample names
+    print("Extracting column names...")
     beta_columns = {}
     for index, value in enumerate(header):
         str="%i %s" % (index, value)
@@ -19,6 +21,7 @@ with open(EPIC_TSV, newline='') as tsvfile:
             sample_name = value.split(".")[0]
             beta_columns[sample_name] = index
 
+    print("\n\nGenerating BED files per sample...")
     # Loop through all rows
     # Columns of interest:
     # - TargetID (1)
@@ -47,14 +50,25 @@ with open(EPIC_TSV, newline='') as tsvfile:
 
 
     # Sort all the bedfiles
+    print("\n\nSorting BED files...")
     for sample, beta_col in beta_columns.items():
         unsorted_bed_file = "%s/%s.unsorted.bed" % (OUT_DIR, sample)
         sorted_bed_file = "%s/%s.sorted.bed" % (OUT_DIR, sample)
 
+        # Add an IGV track line to each bed file, in a random color
+        r = "%s" % random.randint(0,255)
+        g = "%s" % random.randint(0,255)
+        b = "%s" % random.randint(0,255)
+        rgb = ','.join([r,g,b])
+
+        track_line = 'track name=EPIC description="EPIC" graphType="heatmap" color=%s viewLimits=0:1 maxHeightPixels=50:50:50' % rgb
+        with open(sorted_bed_file, 'w') as file:
+            file.write(track_line + '\n')
+
         with open(unsorted_bed_file, 'r') as file:
             lines = file.readlines()
             sorted_lines = sorted(lines, key=lambda x: (x.split('\t')[0], int(x.split('\t')[1])))
-            with open(sorted_bed_file, 'w') as file:
+            with open(sorted_bed_file, 'a') as file:
                 file.writelines(sorted_lines)
 
 
